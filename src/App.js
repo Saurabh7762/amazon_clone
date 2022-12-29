39e047bd9588c05c3ea2bd01d144f6c051e829f3
Saurabh7762 <Saurabhkumars7762@gmail.com>
2022-12-30 04:22:59 +0530

    m

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import Header from './components/Header';
 import Home from './components/Home';
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
-import Checkout from './components/Checkout';
+import Checkout from './components/Checkout_page/Checkout';
 
 function App() {
   return (
